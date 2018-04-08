0d9c0dd986acc54a3875ac8150db2afdd814f3e7
orsonbudd <Orson.budd@gmail.com>
2018-04-08 19:00:07 +0200

    fixed webpack config

--- static/js/4.708cbedf662d91f8c554.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{LTuK:function(e,n){},wUZA:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{})},staticRenderFns:[]};var u=t("VU/8")({},r,!1,function(e){t("LTuK")},null,null);n.default=u.exports}});
-//# sourceMappingURL=4.708cbedf662d91f8c554.js.map
