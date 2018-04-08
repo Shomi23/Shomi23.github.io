ea698de9a8ba1311437726eab1bd3b7d8299a04b
orsonbudd <Orson.budd@gmail.com>
2018-04-08 11:48:40 +0200

    added image-webpack-loader

---
+++ static/js/0.3c106134aeca2fce2dcf.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{nWeO:function(c,f,e){c.exports=e.p+"687c57215b71c7ae20fc0ab5d96b149a.svg"},qdHt:function(c,f,e){c.exports=e.p+"6f7f616f44e4c4244580338661f876d9.svg"}});
+//# sourceMappingURL=0.3c106134aeca2fce2dcf.js.map
--- static/js/0.7a6b3717680f195a2bc3.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],{nWeO:function(t,s,i){t.exports=i.p+"static/img/you.687.svg"},qdHt:function(t,s,i){t.exports=i.p+"static/img/ins.6f7.svg"}});
-//# sourceMappingURL=0.7a6b3717680f195a2bc3.js.map
